2953eb32b6abd82d558705a424138cf7e34dc6eb
Paulpoli1 <pauluni10@hotmail.com>
2020-06-18 11:18:22 +0200

    Task/173376797/Analisi del codice type script

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
+  messaggio = "Sono il componente welcome";
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.messaggio);
   }
 
 }
