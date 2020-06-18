4bf509e6033c4bfe9a95dd3588486b7d721732fb
Paulpoli1 <pauluni10@hotmail.com>
2020-06-18 12:54:49 +0200

    Task/173376797/Sistema autentificazione uso ngIf

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  userid = "Nicola"
+  userid = ""
   password = "";
+  autenticato = true;
+  errorMsg = "Spiacente la userid o la password sono errati!";
 
   constructor() { }
 
@@ -16,8 +18,11 @@ export class LoginComponent implements OnInit {
   }
 
   gestAut(){
-    console.log(this.userid)
-    console.log(this.password)
+    if(this.userid === "Nicola" && this.password ==="123"){
+      this.autenticato = true;
+    }
+    else{
+      this.autenticato = false;
+    }
   }
-
 }
